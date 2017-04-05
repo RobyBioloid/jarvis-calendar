52bfa6e8b5251408ccf052c29a3bfc2ee8f21a93
RobyBioloid <roby.bioloid@gmail.com>
2017-04-05 13:48:27 +0200

    Correction bug de recherche d'événement
    L'option Quand est l'événement "titre de l'événement" est à nouveau fonctionelle

--- python/lookForEvent.py
+++ python/lookForEvent.py
@@ -7,53 +7,95 @@ import os
 from toolsEvent import *
 
 def lookForEvent():
-    '''
-    reply = ""
+    reply = u"Je regarde\n"
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    reply += "Je regarde.\n"
     eventsResult = service.events().list(
-        calendarId='primary', timeMin=now, maxResults=50, singleEvents=True,
+        calendarId='primary', timeMin=now, maxResults=100, singleEvents=True,
         orderBy='startTime').execute()
     events = eventsResult.get('items', [])
         
     file = open(EVENT_PATH)
-    myEvent = file.readlines()
-    summary = myEvent[0].capitalize()
+    myEvent = file.readline() + "\n"
     file.close()
 
+    myEventSplit = myEvent.split()
+    
+    myEventCopy = "" 
+    copy = False
+    i = 0
+    while i < len(myEventSplit) :
+        if copy == True :
+            myEventCopy += myEventSplit[i] + " "
+        else :
+            if myEventSplit[i] == "événement" or myEventSplit[i] == "évènement" or myEventSplit[i] == "l'événement" or myEventSplit[i] == "l'évènement" :
+                copy = True
+        i += 1
+
+    myEventCopy = myEventCopy[0:len(myEventCopy)-1]
+    summary = myEventCopy.lower()
+    summary = unicode(summary, 'utf-8')
     eventFound = False
     for event in events:
-        if summary == event['summary'] +'\n' :
+        try :
+            summ = event['summary'].lower()
+        except :
+            summ = "Sans titre"
+
+        print("summ = " + summ)
+        print("summary = " + summary)
+        if summary == summ :
             eventFound = True
-            debut= event['start'].get('dateTime')
-
-            # Formatage de la date
-            if debut[8:10] == now[8:10] :
-                date = "aujourd'hui"
-            elif int(debut[8:10])-1 == int(now[8:10]) :
-                date = "demain"
-            else :
-                date = "le " + debut[8:10] + " " + get_strMonth(debut[5:7]) + " " + debut[0:4]
             
-            # Formatage de l'heure
-            heure = debut[11:13] + " heure"
-            if debut[14:16] != "00" :
-                heure = heure + " " + debut[14:16]
+            debut = event['start'].get('dateTime')
+            if debut == None :
+                debut = event['start'].get('date')
+                
+                # Formatage de la date
+                if debut[8:10] == now[8:10] :
+                    date = "aujourd'hui"
+                elif int(debut[8:10])-1 == int(now[8:10]) :
+                    date = "demain"
+                else :
+                    date = "le " + str(debut[8:10]) + " " + str(get_strMonth(debut[5:7])) + " " + str(debut[0:4])
+                heure = ""
+                
+            else :
+                # Formatage de la date
+                if debut[8:10] == now[8:10] :
+                    date = "aujourd'hui"
+                elif int(debut[8:10])-1 == int(now[8:10]) :
+                    date = "demain"
+                else :
+                    date = "le " + str(debut[8:10]) + " " + str(get_strMonth(debut[5:7])) + " " + str(debut[0:4])
+            
+                # Formatage de l'heure
+                heure = u" à " + debut[11:13] + " heure"
+                if debut[14:16] != "00" :
+                    heure = heure + " " + debut[14:16]
 
-        
-            fin = event['end'].get('dateTime')
-            location = event['location']
-            description = event['description']
-            reply = reply + u"L'événement " + summary + u" est prévu pour " + date + u" à " + heure + "\n"
+            try :
+                fin = event['end'].get('dateTime')
+            except :
+                fin = date
+            try :
+                location = event['location']
+            except :
+                location = "Sans lieu"
+            try :
+                description = event['description']
+            except :
+                description = "Pas de description"
+                
+            reply = reply + u"L'événement " + summary + u" est prévu pour " + date + heure + "\n"
+            break
             
     if eventFound == False :
-        reply = reply + u"Aucun événement trouvé sous le nom " + summary
-    '''
-    reply = u"L'option de recherche est en cours de debug.\n"
+        reply = reply + u"Aucun événement trouvé sous le nom " + summary + "\n"
+
     file = open(EVENT_PATH, "w")
     file.write(reply.encode('utf-8'))
     file.close()
